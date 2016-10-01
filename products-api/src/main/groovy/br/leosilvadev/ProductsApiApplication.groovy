ca8d02c2f581e641ec347f27ce97cd118b8fbdc5
Leonardo <leosilvadev@gmail.com>
2016-10-01 23:48:19 +0200

    config products as an api for eureka

--- products-api/src/main/groovy/br/leosilvadev/ProductsApiApplication.groovy
+++ products-api/src/main/groovy/br/leosilvadev/ProductsApiApplication.groovy
@@ -2,8 +2,10 @@ package br.leosilvadev
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 class ProductsApiApplication {
 
 	static void main(String[] args) {
