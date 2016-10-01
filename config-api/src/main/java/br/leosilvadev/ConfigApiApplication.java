7c1e4458c03592970d7c36411e34cae6936cf7ec
Leonardo <leosilvadev@gmail.com>
2016-10-01 21:32:48 +0200

    config api created

---
+++ config-api/src/main/java/br/leosilvadev/ConfigApiApplication.java
@@ -0,0 +1,12 @@
+package br.leosilvadev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApiApplication.class, args);
+	}
+}
