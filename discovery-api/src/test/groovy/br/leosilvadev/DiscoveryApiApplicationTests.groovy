3f72c635947a738ba8d88b9d7680a32aa7fe0281
Leonardo <leosilvadev@gmail.com>
2016-10-01 23:48:03 +0200

    discovery api

---
+++ discovery-api/src/test/groovy/br/leosilvadev/DiscoveryApiApplicationTests.groovy
@@ -0,0 +1,16 @@
+package br.leosilvadev
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class DiscoveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
