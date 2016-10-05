454cba18af0eb3ce45463304480e5d0709c1cff0
Leonardo <leosilvadev@gmail.com>
2016-10-05 23:46:57 +0200

    plans api project

---
+++ plans-api/src/main/groovy/br/leosilvadev/plans/Application.groovy
@@ -0,0 +1,12 @@
+package br.leosilvadev.plans
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application {
+
+	static void main(args) {
+		SpringApplication.run Application, args
+	}
+}
