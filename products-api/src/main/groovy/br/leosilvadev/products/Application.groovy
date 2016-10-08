9abef16443ba3cf812c415dd82c57ee47a4faf1f
Leonardo <leosilvadev@gmail.com>
2016-10-09 00:16:36 +0200

    hystrix configç

--- products-api/src/main/groovy/br/leosilvadev/products/Application.groovy
+++ products-api/src/main/groovy/br/leosilvadev/products/Application.groovy
@@ -1,15 +1,17 @@
-package br.leosilvadev.products;
+package br.leosilvadev.products
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.netflix.feign.EnableFeignClients
+import org.springframework.cloud.netflix.hystrix.EnableHystrix
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
-@EnableCircuitBreaker
+@EnableHystrix
+@EnableHystrixDashboard
 class Application {
 
 	static void main(String[] args) {
--- products-api/src/main/groovy/br/leosilvadev/products/product/services/ProductRegistrar.groovy
+++ products-api/src/main/groovy/br/leosilvadev/products/product/services/ProductRegistrar.groovy
@@ -3,14 +3,15 @@ package br.leosilvadev.products.product.services
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
-import rx.Observable
 import br.leosilvadev.products.establishment.clients.v1.EstablishmentClient
 import br.leosilvadev.products.product.domains.Product
 import br.leosilvadev.products.product.repositories.ProductRepository
 import br.leosilvadev.products.utils.React
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty
 
+import rx.Observable
 
 @Service
 class ProductRegistrar {
@@ -21,7 +22,11 @@ class ProductRegistrar {
 	@Autowired
 	EstablishmentClient establishmentClient
 
-	@HystrixCommand
+	@HystrixCommand(groupKey='product-register',
+		commandProperties = [
+			@HystrixProperty(name="execution.isolation.strategy", value="SEMAPHORE")
+		]
+	)
 	Observable<Product> register(Product product) {
 		React.when {
 			(validate >> save)(product)
